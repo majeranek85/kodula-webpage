d3bf3df351017b484ff70af8535f1392078d9e47
Marta Majerowska <majeranek85@gmail.com>
2021-02-03 22:50:33 +0100

    Setup page typography

---
+++ src/components/layout.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import '../styles/normalize.css';
+import '@fontsource/lato';
+import '@fontsource/orbitron';
+
+const GlobalStyle = createGlobalStyle`
+
+  /* Document level adjustments */
+  html {
+    font-size: 18px;
+    box-sizing: border-box;
+    font-family: lato;
+  }
+
+  body {
+    background: gray;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin: 0;
+    font-weight: 300;
+  }
+
+  h1 {
+    margin-top: 0;
+  }
+
+  @media (max-width: 900px) {
+    html { font-size: 16px; }
+  }
+
+  @media (max-width: 400px) {
+    html { font-size: 14px; }
+  }
+
+  /* Modules will scale with document */
+  .header {
+    font-size: 1.5rem;
+  }
+
+  .footer {
+    font-size: 0.75rem;
+  }
+
+  .sidebar {
+    font-size: 0.85rem;
+  }
+
+  /* Type will scale with modules */
+  h1 {
+    font-size: 3em;
+  }
+  h2 {
+    font-size: 2.5em;
+  }
+  h3 {
+    font-size: 2em;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+
+
+  h1, h2, h3, h4, h5, span {
+    margin: 0;
+  }
+
+  h1, h2, h3, h4, h5 {
+    font-family: orbitron;
+    font-weight: 500;
+  }
+
+  button {
+    padding: 0;
+    cursor: pointer;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+
+  p {
+    line-height: 1.45;
+  }
+`;
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <GlobalStyle />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
-import React from "react"
+import React from 'react';
+import Layout from '../components/layout';
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Layout>
+      <h1>Hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <h2>H2 Hello world</h2>
+      <h3>H3 Hello world</h3>
+      <ul>
+        <li>text</li>
+        <li>text</li>
+      </ul>
+
+      <button> lalala </button>
+    </Layout>
+  );
 }
