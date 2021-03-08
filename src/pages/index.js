b4e1776316d923b98c65a047cd4d5f0ba8679c81
Marta Majerowska <majeranek85@gmail.com>
2021-03-08 21:46:39 +0100

    Add styleguide page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import Layout from '../components/layout';
-import Button from '../components/common/button';
+//import Button from '../components/common/button';
 import Tabs from '../components/common/tabs';
 
 /* DATA */
-
 const services = [
   {
     name: 'strony internetowe',
@@ -20,35 +19,19 @@ const services = [
   },
 ];
 
-export default function Home() {
+const Home = () => {
   return (
     <Layout>
-      <h1>Hello world</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-        sunt in culpa qui officia deserunt mollit <a href="\">lumpek</a> anim id est laborum.
-      </p>
-      <h2>H2 Hello world</h2>
-      <h3>H3 Hello world</h3>
-      <ul>
-        <li>text</li>
-        <li>text</li>
-      </ul>
-
-      <Button> zamawiam </Button>
-      <Button light> zamawiam </Button>
-      <Button social> f </Button>
-      <Button social light> In </Button>
-
-      <Tabs>
+      {/*<Tabs>
         {services.map((service) => (
           <article key={service.name} label={service.name}>
             <h2>{service.name}</h2>
             <p>{service.description}</p>
           </article>
         ))}
-      </Tabs>
+      </Tabs>*/}
     </Layout>
   );
 }
+
+export default Home;
---
+++ src/pages/styleguide.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Layout from '../components/layout';
+import Button from '../components/common/button';
+import Tabs from '../components/common/tabs';
+
+/* DATA */
+
+const services = [
+  {
+    name: 'strony internetowe',
+    description: 'Lorem ipsum Strony internetowe',
+  },
+  {
+    name: 'pozycjonowanie',
+    description: 'Lorem ipsum Pozycjonowanie',
+  },
+  {
+    name: 'social media',
+    description: 'Lorem ipsum Social media',
+  },
+];
+
+const Styleguide = () => {
+  return (
+    <Layout>
+      <h1>Hello world</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+        sunt in culpa qui officia deserunt mollit <a href="\">lumpek</a> anim id est laborum.
+      </p>
+      <h2>H2 Hello world</h2>
+      <h3>H3 Hello world</h3>
+      <ul>
+        <li>text</li>
+        <li>text</li>
+      </ul>
+
+      <Button> zamawiam </Button>
+      <Button light> zamawiam </Button>
+      <Button social> f </Button>
+      <Button social light> In </Button>
+
+      <Tabs>
+        {services.map((service) => (
+          <article key={service.name} label={service.name}>
+            <h2>{service.name}</h2>
+            <p>{service.description}</p>
+          </article>
+        ))}
+      </Tabs>
+    </Layout>
+  );
+}
+
+export default Styleguide;
