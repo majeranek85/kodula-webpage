bf2d1809d4d39f556bb4ae5c5a101c9c4016d11f
Marta Majerowska <majeranek85@gmail.com>
2021-06-09 22:04:28 +0200

    Add bootstrap

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../styles/globalStyles';
 import colors from '../utils/colors';
 import Header from '../components/layout/header'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Layout = ({ children }) => {
   return (
