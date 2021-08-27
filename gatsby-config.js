31c1f84881e4bbc1d27fddd333348fa1b61ad4e8
Marta Majerowska <majeranek85@gmail.com>
2021-08-27 14:31:32 +0200

    Install packages to use gatsby-image

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,20 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+const path = require(`path`)
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: path.join(__dirname, `src`, `images`),
+      },
+    },
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 };
