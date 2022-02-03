9dce8cf64bb0e691143a33923077c863a5208001
Marta Majerowska <majeranek85@gmail.com>
2022-02-03 15:52:26 +0100

    Add images to our work section

--- src/components/sections/ourWork.js
+++ src/components/sections/ourWork.js
@@ -1,49 +1,47 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import Img from 'gatsby-image';
 import { Container, Row, Col } from 'react-bootstrap';
 import Section from '../layout/section';
 import styled from 'styled-components';
-import Button from '../common/button';
+//import Button from '../common/button';
 //import theme from '../../utils/theme';
 
-/* DATA */
-
-const data = [
-  {
-    name: 'name',
-    url: 'http//scarlet-uroda.pl',
-    title: 'title',
-    alt: 'scarlet',
-  },
-  {
-    name: 'name',
-    url: 'http//',
-    title: 'title',
-    alt: 'alter',
-  },
-  {
-    name: 'name',
-    url: 'http//',
-    title: 'title',
-    alt: 'alter',
-  },
-];
-
 const OurWork = () => {
+
+  /* DATA */
+
+  const data = useStaticQuery(graphql`
+  {
+    allImageSharp {
+      nodes {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+  `)
+
+  console.log(data);
+
+  const images = data.allImageSharp.nodes
+
   return (
     <Section bgLightSecond>
       <Container>
         <StyledTitle>NASZE REALIZACJE</StyledTitle>
         <Row>
-          {data.map((item, id) => (
+          {images.map((item, id) => (
             <Col xs={12} md={4} key={id}>
               <StyledFigure>
-                <img src={item.url} alt={item.alt} />
-                <figcaption>{item.name}</figcaption>
+              <Img fluid={item.fluid} alt="hero" />
+                {/* <figcaption>{item.name}</figcaption> */}
               </StyledFigure>
             </Col>
           ))}
         </Row>
-        <StyledBtn>ZOBACZ WIĘCEJ</StyledBtn>
+        {/* <StyledBtn>ZOBACZ DALEJ</StyledBtn> */}
       </Container>
     </Section>
   );
@@ -63,7 +61,7 @@ const StyledFigure = styled.figure`
   margin: 1em;
 `;
 
-const StyledBtn = styled(Button)`
-  margin: 3rem auto;
-  background: transparent;
-`;
+// const StyledBtn = styled(Button)`
+//   margin: 3rem auto;
+//   background: transparent;
+// `;
