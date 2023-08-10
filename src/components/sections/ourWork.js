49ab27582e60f399a7e9fc920a5981f6dfa84c52
Marta Majerowska <majeranek85@gmail.com>
2023-08-10 21:09:36 +0200

    Fix image data query

--- src/components/sections/ourWork.js
+++ src/components/sections/ourWork.js
@@ -4,8 +4,8 @@ import Img from 'gatsby-image';
 import { Container, Row, Col } from 'react-bootstrap';
 import Section from '../layout/section';
 import styled from 'styled-components';
-//import Button from '../common/button';
-//import theme from '../../utils/theme';
+import Button from '../common/button';
+import theme from '../../utils/theme';
 
 const OurWork = () => {
 
@@ -13,10 +13,16 @@ const OurWork = () => {
 
   const data = useStaticQuery(graphql`
   {
-    allImageSharp {
-      nodes {
-        fluid {
-          ...GatsbyImageSharpFluid
+    allFile(filter: {relativeDirectory: {eq: "portfolio"}}) {
+      edges {
+        node {
+          id
+          childImageSharp {
+            id
+            fluid {
+              ...GatsbyImageSharpFluid
+            }
+          }
         }
       }
     }
@@ -25,7 +31,7 @@ const OurWork = () => {
 
   console.log(data);
 
-  const images = data.allImageSharp.nodes
+  const images = data.allFile.edges
 
   return (
     <Section bgLightSecond>
@@ -33,15 +39,15 @@ const OurWork = () => {
         <StyledTitle>NASZE REALIZACJE</StyledTitle>
         <Row>
           {images.map((item, id) => (
-            <Col xs={12} md={4} key={id}>
+            <Col xs={12} md={6} key={id}>
               <StyledFigure>
-              <Img fluid={item.fluid} alt="hero" />
+              <Img fluid={item.node.childImageSharp.fluid} alt="hero" />
                 {/* <figcaption>{item.name}</figcaption> */}
               </StyledFigure>
             </Col>
           ))}
         </Row>
-        {/* <StyledBtn>ZOBACZ DALEJ</StyledBtn> */}
+        <StyledBtn>ZOBACZ DALEJ</StyledBtn>
       </Container>
     </Section>
   );
@@ -57,11 +63,11 @@ const StyledTitle = styled.h2`
 
 const StyledFigure = styled.figure`
   background: lightgray;
-  height: 276px;
-  margin: 1em;
 `;
 
-// const StyledBtn = styled(Button)`
-//   margin: 3rem auto;
-//   background: transparent;
-// `;
+const StyledBtn = styled(Button)`
+  margin: 3rem auto;
+  :hover {
+    color: ${theme.secondaryLight};
+  }
+`;
